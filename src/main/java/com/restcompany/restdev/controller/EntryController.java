38efa783e6fe907a52b0bd5b6d1327f36562860e
Raunak Kumar <raunakkh8789@gmail.com>
2026-07-07 15:03:20 +0530

    some update

--- src/main/java/com/restcompany/restdev/controller/EntryController.java
+++ src/main/java/com/restcompany/restdev/controller/EntryController.java
@@ -3,12 +3,15 @@ package com.restcompany.restdev.controller;
 import com.restcompany.restdev.entity.Entry;
 import com.restcompany.restdev.service.EntryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/entries")
@@ -27,9 +30,12 @@ public class EntryController {
      return true;
     }
     @GetMapping("id/{id}")
-    public Entry getById(@PathVariable String id){
-        return entryService.findById(id).orElse(null);
-
+    public ResponseEntity<?> getById(@PathVariable String id){
+        Optional<Entry> entries = entryService.findById(id);
+        if(entries.isPresent()){
+            return new ResponseEntity<>(entries.get(),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
