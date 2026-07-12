1a6e2e4dd11c10c3c2017c107931e820e26e4e58
Raunak Kumar <raunakkh8789@gmail.com>
2026-07-12 21:02:05 +0530

    just daily routien

--- src/main/java/com/restcompany/restdev/RestdevApplication.java
+++ src/main/java/com/restcompany/restdev/RestdevApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestdevApplication {
+public class RestdevApplication { //his is an main application in java
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestdevApplication.class, args);
